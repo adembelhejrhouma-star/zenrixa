7285b3fc073b9db4c4500d5c96007858e126e050
adem bhr <adembelhejrhouma@gmail.com>
2026-08-14 18:51:30 +0100

    fix safe middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,10 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
 }
 
 export const config = {
-  runtime: 'edge',//
-  matcher: [
-    '/',           //
-    '/login',
-    '/signup',
-    '/landing',    // ou landing.html
-    '/account/:path*',
-    '/dashboard/:path*',
-    '/checkout',
-    '/checkout/:path*',
-    '/api/auth/:path*',
-  ],
-}
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
