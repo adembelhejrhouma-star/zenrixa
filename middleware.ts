e4f95ba2ef6737b601a5fdf97c41633411f537b4
adem bhr <adembelhejrhouma@gmail.com>
2026-08-14 18:38:41 +0100

    fix: change middleware runtime to edge

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: 'nodejs',//
+  runtime: 'edge',//
   matcher: [
     '/',           //
     '/login',
